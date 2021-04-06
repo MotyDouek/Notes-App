dca03b85c7930e07808270db030c9569201ddfe8
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-04-06 13:06:56 +0300

    using the require meth

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-//fs.writeFileSync('notes.txt', 'My name is Moty.');
-fs.appendFileSync('notes.txt', " I am from Ramat Gan.");
+// //fs.writeFileSync('notes.txt', 'My name is Moty.');
+// fs.appendFileSync('notes.txt', " I am from Ramat Gan.");
+
+const getNotes = require('./notes.js');
+const msg = getNotes();
+console.log(msg);
