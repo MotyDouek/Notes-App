8818e175c71f29a5f3da3337eba86fd0948a9dfd
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-04-06 17:56:15 +0300

    npm validator

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 // //fs.writeFileSync('notes.txt', 'My name is Moty.');
 // fs.appendFileSync('notes.txt', " I am from Ramat Gan.");
 
+const validator = require('validator');
 const getNotes = require('./notes.js');
 const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isURL('http://mead.io'))
