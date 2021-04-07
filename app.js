0dd90356c15363590703f83c0fb6c7d508eece33
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-04-07 11:19:33 +0300

    chalk lib

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 // //fs.writeFileSync('notes.txt', 'My name is Moty.');
 // fs.appendFileSync('notes.txt', " I am from Ramat Gan.");
 
+const chalk = require('chalk');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 const msg = getNotes();
 console.log(msg);
 
 console.log(validator.isURL('http://mead.io'))
+
+console.log(chalk.green.bold.inverse('Success!'));
