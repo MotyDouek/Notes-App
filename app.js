84104a7f83196796e4f08bd5e624f3aefbbb40b5
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-04-09 16:52:32 +0300

    npm yargs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // fs.appendFileSync('notes.txt', " I am from Ramat Gan.");
 
 const chalk = require('chalk');
+const yargs = require('yargs');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 
@@ -18,6 +19,7 @@ console.log(chalk.green.bold.inverse('Success!'));
 console.log(process.argv[2]);
 */
 
+/*
 const command = process.argv[2];
 
 console.log(process.argv);
@@ -27,3 +29,45 @@ if (command == 'add') {
 } else if (command == 'remove') {
     console.log('Removing note!');
 }
+*/
+
+//Customize yargs version
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note');
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listting the notes');
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+})
+
+console.log(yargs.argv);
